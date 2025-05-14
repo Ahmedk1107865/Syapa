a40103e9ddbc0129646b5d06caa4f5b53d9cdda1
SHAYAN273 <laiqkhan21708@gmail.com>
2025-05-14 08:28:52 +0500

    Update uid.js

--- Priyansh/commands/uid.js
+++ Priyansh/commands/uid.js
@@ -8,10 +8,20 @@ module.exports.config = {
 	cooldowns: 5
 };
 
+module.exports.config = {
+	name: "uid",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Zeeshan Altaf",
+	description: "Dont Change This Credits Otherwisw Your Bot Lol",
+	commandCategory: "other",
+	cooldowns: 5
+};
+
 module.exports.run = function({ api, event }) {
-	if (Object.keys(event.mentions) == 0) return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);
+	if (Object.keys(event.mentions) == 0) return api.sendMessage(`ye lo  ye hai ap ke user id: \n${event.senderID}\n\n ye ap ka messenger chat link:\nm.me/${event.senderID}\n\n ye ap ka facebook profile link:\nwww.facebook.com/${event.senderID} *★᭄𝗖𝗿𝗲𝗱𝗶𝘁𝘀  ཫ༄𒁍≛⃝𝐒𝐘𝐀𝐏𝐀 😒🥂✨`, event.threadID, event.messageID);
 	else {
 		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.values(event.mentions)[i].replace('@', '')}: ${Object.keys(event.mentions)[i]}`, event.threadID);
 		return;
 	}
-}
+}          
