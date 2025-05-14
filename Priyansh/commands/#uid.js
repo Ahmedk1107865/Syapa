6349808515951376d166d3b389adad33ecdd9af2
SHAYAN273 <laiqkhan21708@gmail.com>
2025-05-14 08:31:53 +0500

    Update #uid.js

--- Priyansh/commands/#uid.js
+++ Priyansh/commands/#uid.js
@@ -9,47 +9,20 @@ module.exports.config = {
  cooldowns: 5
 };
 
-module.exports.run = async function({ event, api, args, client, Currencies, Users, utils, __GLOBAL, reminder }) {
-const fs = global.nodemodule["fs-extra"];
-		const request = global.nodemodule["request"];
-		const axios = global.nodemodule['axios']; 
-		if(event.type == "message_reply") { 
-			let name = await Users.getNameUser(event.messageReply.senderID) 
- uid = event.messageReply.senderID
- var callback = () =>   api.sendMessage({body:`=== [ 𝗨𝗜𝗗 𝗨𝗦𝗘𝗥 ] ====\n━━━━━━━━━━━━━━━━━━\n[ ▶️]➜ 𝗜𝗗: ${uid}\n[ ▶️]➜ 𝗜𝗕: m.me/${uid}\n[ ▶️]➜ 𝗟𝗶𝗻𝗸𝗳𝗯: https://www.facebook.com/profile.php?id=${uid}\n━━━━━━━━━━━━━━━━━━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
-				() => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
-		return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
-				() => callback()); 
-		}
-		if (!args[0]) {
-			var uid = event.senderID;
-			const res = await axios.get(`https://www.nguyenmanh.name.vn/api/fbInfo?id=${uid}&apikey=LV7LWgAp`);
-var name = res.data.result.name 
-				var callback = () =>  api.sendMessage({body:`=== [ 𝗨𝗜𝗗 𝗨𝗦𝗘𝗥 ] ====\n━━━━━━━━━━━━━━━━━━\n[ ▶️]➜ 𝗜𝗗: ${event.senderID}\n[ ▶️]➜ 𝗜𝗕: m.me/${event.senderID}\n[ ▶️]➜ 𝗟𝗶𝗻𝗸𝗳𝗯: https://www.facebook.com/profile.php?id=${event.senderID}\n━━━━━━━━━━━━━━━━━━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
-				() => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
-		return request(encodeURI(`https://graph.facebook.com/${event.senderID}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
-				() => callback()); 
-		}
-		else {
- if (args[0].indexOf(".com/")!==-1) {
-		const res_ID = await api.getUID(args[0]);
-	 var name = data.name
-var data = await api.getUserInfoV2(res_ID);
-		var username = data.username
-		var link = data.link
-		var callback = () => api.sendMessage({body:`=== [ 𝗨𝗜𝗗 𝗨𝗦𝗘𝗥 ] ====\n━━━━━━━━━━━━━━━━━━\n[ ▶️]➜ 𝗜𝗗: ${res_ID}\n[ ▶️]➜ 𝗜𝗕: m.me/${res_ID}\n[ ▶️]➜ 𝗟𝗶𝗻𝗸𝗳𝗯: ${link}\n━━━━━━━━━━━━━━━━━━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
-				() => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
-		return request(encodeURI(`https://graph.facebook.com/${res_ID}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
-				() => callback()); }
- else {
-	if (args.join().indexOf('@') !== -1) 
-			var uid = Object.keys(event.mentions) 
-			var callback = () => 
-api.sendMessage({body:`=== [ 𝗨𝗜𝗗 𝗨𝗦𝗘𝗥 ] ====\n━━━━━━━━━━━━━━━━━━\n[ ▶️]➜ 𝗜𝗗: ${uid}\n[ ▶️]➜ 𝗜𝗕: m.me/${uid}\n[ ▶️]➜ 𝗟𝗶𝗻𝗸𝗳𝗯: https://www.facebook.com/profile.php?id=${uid}\n━━━━━━━━━━━━━━━━━━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
-				() => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
-		return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
-				() => callback()); 
+module.exports.config = {
+	name: "uid",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Zeeshan Altaf",
+	description: "Dont Change This Credits Otherwisw Your Bot Lol",
+	commandCategory: "other",
+	cooldowns: 5
+};
 
- }
-}
-}
+module.exports.run = function({ api, event }) {
+	if (Object.keys(event.mentions) == 0) return api.sendMessage(`ye lo  ye hai ap ke user id: \n${event.senderID}\n\n ye ap ka messenger chat link:\nm.me/${event.senderID}\n\n ye ap ka facebook profile link:\nwww.facebook.com/${event.senderID} *★᭄𝗖𝗿𝗲𝗱𝗶𝘁𝘀  ཫ༄𒁍≛⃝𝐒𝐘𝐀𝐏𝐀 😒🥂✨`, event.threadID, event.messageID);
+	else {
+		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.values(event.mentions)[i].replace('@', '')}: ${Object.keys(event.mentions)[i]}`, event.threadID);
+		return;
+	}
+}          
